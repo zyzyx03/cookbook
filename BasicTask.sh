f1a0eec9b2782c78291b7d983d5a1a15863d8d69
zyzyx03 <fauzuwan.nazri93@gmail.com>
2019-04-21 19:02:31 +0800

    Basic Task Script

---
+++ BasicTask.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Author: zyzyx03
+# Date Created: 21/4/2019
+# Description: Run basic linux tasks
+# Date Modified: 21/4/2019
+
+echo 
+echo "This will run a few basic task"
+echo
+pwd
+echo 
+ls
+echo
+echo 
+whoami
+echo 
+date
+echo 
+cal
+echo "End of script" 
