86816a87ac7c1715b9a82cf52f32b73560a70406
zyzyx03 <fauzuwan.nazri93@gmail.com>
2019-11-17 12:11:54 +0800

    Node.Js course completed

---
+++ Node.js/reference/fs_demo.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create Folder
+// fs.mkdir(path.join(__dirname, '/test'), {}, (err) => {
+//     if(err) throw err;
+//         console.log('Folder created');
+// });
+
+// Create and Write to file 
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
+//     if(err) throw err;
+//         console.log('File written to...');
+
+// Append file text
+// fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello Dunia!', err => {
+//     if(err) throw err;
+//         console.log('File written to...');
+// });
+// });
+
+// Read file
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data)
+// })
+
+// Rename a file
+// fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), (err) => {
+//     if(err) throw err;
+//         console.log('File renamed');
+// });
+
+
+
+
+
