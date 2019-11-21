1df1cae50e6054e37e104ad766b752dcdf28079f
Fauzuwan Nazri <fauzuwan@newsoft.com.my>
2019-11-21 15:37:10 +0800

    Completed terraform simple compute on GCP

--- Terraform/GCP-terraform/variable.tf
+++ Terraform/GCP-terraform/variable.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
- credentials = "${file("CREDENTIALS_FILE.json")}"
- project     = "flask-app-211918"
- region      = "us-west1"
+ credentials = "black-dragon-256712-ffe3b3e87d27.json"
+ project     = "black-dragon-256712"
+ region      = "asia-southeast1"
 }
